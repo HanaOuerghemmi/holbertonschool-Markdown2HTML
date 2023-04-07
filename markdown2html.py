e4a6e7b7560257e373559173a86b714219915f49
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-04-07 22:38:00 +0100

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
-'''
-a script markdown2html.py that takes an argument 2 strings:
-First argument is the name of the Markdown file
-Second argument is the output file name
+''' a script markdown2html.py that takes an argument 2 strings:
+    First argument is the name of the Markdown file
+    Second argument is the output file name
 '''
 
 import sys
 import os
 import markdown
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
 
     if len(sys.argv) < 3:
-     print("Usage: ./markdown2html.py README.md README.html")
-     sys.exit(1)
+        print("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
@@ -30,4 +29,3 @@ if __name__ == '__main__':
             f.write(html)
     else:
         sys.exit(0)
-
