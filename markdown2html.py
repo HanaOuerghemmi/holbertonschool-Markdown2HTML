cae257d9d33f75fdf13fb732cc9d4c870c257f4d
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-04-07 22:51:48 +0100

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-''' a script markdown2html.py that takes an argument 2 strings:
-    First argument is the name of the Markdown file
-    Second argument is the output file name
-'''
+"""a script markdown2html.py"""
 
 import sys
 import os
 import markdown
 
 if __name__ == '__main__':
+    """a script markdown2html.py"""
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
