c5895625f4516279417ee1a7e18dc7886b061749
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-04-07 22:41:35 +0100

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 ''' a script markdown2html.py that takes an argument 2 strings:
     First argument is the name of the Markdown file
     Second argument is the output file name
@@ -10,16 +9,15 @@ import os
 import markdown
 
 if __name__ == '__main__':
-
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
-        sys.exit(1)
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     if not os.path.exists(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
         with open(input_file, 'r') as f:
             text = f.read()
